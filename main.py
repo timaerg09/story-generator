ad857a57718c5f49ffad30d4ef68d4be15f6a67b
Temur <temurergashev070809@gmail.com>
2026-01-03 23:27:26 +0500

    many files

---
+++ main.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
+
+app = FastAPI(
+    title="Choose Your Own Adventure Game API",
+    description="api to generate cool stories",
+    version="0.1.0",
+    docs_url="/",
+    redoc_url="/redoc",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
